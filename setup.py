c2976f0b12bec4c1c202968fe4f5c651422e719d
Nazar Polishchuk <nazarr.polishchuk@gmail.com>
2024-10-15 11:36:09 +0300

    Fixed timm version to 1.0.9

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
   name='coatlib',
   version='0.1.0',
   packages=find_packages(include=['coatlib', 'coatlib.*']),
-  install_requires=['timm', 'einops']
+  install_requires=['timm == 1.0.9', 'einops']
 )
